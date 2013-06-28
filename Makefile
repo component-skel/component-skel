108955692541679723850734e6edcf8ddf52eefe
Jared Forsyth <jared@jaredforsyth.com>
2013-06-28 12:07:56 -0600

    makefile merging works! not super happy w/ it though. Should be its own module

--- Makefile
+++ Makefile
@@ -1,6 +1,6 @@
 
 test:
-	@mocha
+	@mocha -R spec
 
 .PHONY: test
 
--- test/test-make.js
+++ test/test-make.js
@@ -7,30 +7,43 @@ var expect = require('chai').expect
 
 // , splitFixtures = require('split-fixtures');
 
-function loadFixture(fname) {
+function processSplit(fname, parts) {
+  var rules = yaml.safeLoad(parts[1], {
+    filename: fname,
+    strict: true
+  });
+  for (var key in rules) {
+    rules[key] = rules[key].split('\n');
+  }
+  return [parts[0], rules];
+}
+
+function loadFixtures(fname, process) {
   var text = fs.readFileSync(path.join(__dirname, fname))
     .toString('utf8').split('\n===\n');
   return text.map(function (chunk, i) {
     var parts = chunk.split('\n---\n')
-      , rules = yaml.safeLoad(parts[1], {
-          filename: fname + '::' + i,
-          strict: true
-        });
-    for (var key in rules) {
-      rules[key] = rules[key].split('\n');
-    }
-    return [parts[0], rules];
+    return process ? process(fname + '::' + i, parts) : parts;
   });
 }
 
 var fixtures = {
-  split: loadFixture('split-fixtures.yaml')
+  split: loadFixtures('split-fixtures.yaml', processSplit),
+  merge: loadFixtures('merge-fixtures.yaml')
 };
 
 describe('splitMakes', function () {
   fixtures.split.forEach(function (item, i) {
     it('should split fixture ' + i + ' correctly', function () {
       expect(make.split(item[0]).rules).to.eql(item[1]);
+    });
+  });
+});
+
+describe('merge', function () {
+  fixtures.merge.forEach(function (item, i) {
+    it('should merge fixture ' + i + ' correctly', function () {
+      expect(make.merge(item[0], item[1]).trim()).to.equal(item[2].trim());
     });
   });
 });
